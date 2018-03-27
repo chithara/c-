ad77669065d089d0729d7d6dc6ddf1f66422e20d
chithara <37831192+chithara@users.noreply.github.com>
2018-03-27 14:51:21 +0530

    Create hello world .c

---
+++ beginner level/hello world .c
@@ -0,0 +1,15 @@
+{
+	int a, b,c;
+	printf("enter the a and b values");
+	scanf("%d%d",&a&b);
+	printf("enter the c values");
+	scanf("%d",&c);
+	if(a>b && a>c){
+		printf("a is greater");}
+		else if(b>c && b>a)
+		{
+			printf("b is greater");}
+			else 
+			printf("c is greater");
+		
+	}
